c695374720c02fd091d2084ac12a8589eac068f1
alex-gab <dragoi.alex@gmail.com>
2016-11-27 19:30:55 +0200

    [#1]: Initial commit

--- src/main/java/org/dbunit/builder/Row.java
+++ src/main/java/org/dbunit/builder/Row.java
@@ -1,9 +1,10 @@
 package org.dbunit.builder;
 
+import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.builder.DataSetBuilder;
 
 public interface Row {
     String getTableName();
 
-    DataSetBuilder addThisToDataSet(DataSetBuilder dataSetBuilder);
+    DataSetBuilder addThisToDataSet(DataSetBuilder dataSetBuilder) throws DataSetException;
 }
